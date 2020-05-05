b3b951abf434b2fb228cb6be8bec9be6f4221a02
Rajesh <rajeshkannang87@gmail.com>
2020-05-05 12:56:26 +0530

    Access Modifier Example

---
+++ src/com/rajesh/sample/AccessModifier.kt
@@ -0,0 +1,38 @@
+package com.rajesh.sample
+
+fun main(args: Array<String>){
+    val emp1 = Employee("Rajesh", 100)
+    println(emp1)
+
+    val emp2 = Employee("Lenin", 200, true)
+    println(emp2)
+
+    val car = Car("Honda", "Civic", 2014)
+    println(car)
+
+    val car2 = Car("Honda", "Civic", 2014)
+    println(car == car2)
+
+    val car3 = car.copy(model = "Accord")
+    println(car3)
+}
+
+//class Employee constructor(name: String, id: Int){
+//    val name: String = name
+//    val id: Int = id
+//}
+
+class Employee constructor(val name: String, val id: Int){
+
+    var fullTime:Boolean ?= null
+
+    constructor(name: String, id: Int, fullTime: Boolean): this(name, id){
+        this.fullTime = fullTime
+    }
+
+    override fun toString(): String {
+        return "Employee($name, $id, $fullTime)"
+    }
+}
+
+data class Car(val make: String, val model: String, val year: Int){}
