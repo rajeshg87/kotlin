0eb3be8f1f67391498190258c3c601d3d05aa803
Rajesh <rajeshkannang87@gmail.com>
2020-05-05 10:00:38 +0530

    Initial Commit

---
+++ src/com/rajesh/sample/DataTypes.kt
@@ -0,0 +1,16 @@
+package com.rajesh.sample
+
+import java.math.BigDecimal
+
+fun main(args: Array<String>){
+    val int_i: Int = 10
+    var long_i = int_i.toLong()
+    println(long_i is Long)
+
+    val evenNumbers = Array(10){i -> i*2 }
+    println(evenNumbers)
+    evenNumbers.forEach { number -> println(number) }
+
+    val mixedArray = arrayOf(10, 200L, 10.25, "Rajesh", 'A', BigDecimal(1234.98))
+    mixedArray.forEach { value -> println(value) }
+}
---
+++ src/com/rajesh/sample/Declaration.kt
@@ -0,0 +1,40 @@
+package com.rajesh.sample
+
+typealias UserSet = Set<User>
+
+fun main(args: Array<String>){
+    val number: Int
+    var name: String
+    number = 10
+    name = "Rajesh"
+    name = "Kannan"
+    println(String.format("Name : %s Number : %s",name, number))
+
+    val names = arrayListOf("Rajesh", "Lenin", "Aravind", "Vasanth")
+    println(names)
+
+    val user1 = User("Rajesh", 100)
+    val user2 = User("Lenin", 200)
+    val user3 = User("Lenin", 200)
+
+    println(user1 == user2)
+    println(user2 == user3)
+    println(user2 === user3)
+
+    println(user1)
+
+}
+
+class User(var name: String, val id: Int){
+
+    override fun equals(obj: Any?): Boolean {
+        if(obj is User){
+            return name == obj.name && id == obj.id
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return "name=$name, id=$id"
+    }
+}
