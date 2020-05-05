1850205b542d232db7b387db258c46f36f461c40
Rajesh <rajeshkannang87@gmail.com>
2020-05-05 21:01:53 +0530

    Updated Inheritance Method call

--- src/com/rajesh/kotlin/InheritanceFirst.kt
+++ src/com/rajesh/kotlin/InheritanceFirst.kt
@@ -1,16 +1,17 @@
 package com.rajesh.kotlin
 
 fun main(args: Array<String>){
-    val printer = InkJetPrinter("HP Desktop", 12.23)
+    val printer = InkJetPrinter("HP Desktop", 30.50)
+    printer.printerPrice();
 }
 
 abstract class Printer(val modelName: String){
     open fun printModelName() = println("The name of the Printer is $modelName")
-    abstract fun printerPrice(price: Double)
+    abstract fun printerPrice()
 }
 
 class InkJetPrinter(modelName: String, val price: Double): Printer(modelName){
     override  fun printModelName() = println("The name of the InkJet Printer is $modelName")
-    override fun printerPrice(price: Double) = println("Printer Price = $price")
+    override fun printerPrice() = println("Printer Price = $price")
 
 }
