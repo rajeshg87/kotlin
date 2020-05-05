74b8406aa653c7191fa1521e6bc6b00ebf4fac73
Rajesh <rajeshkannang87@gmail.com>
2020-05-05 12:23:30 +0530

    Initial Commit

--- src/com/rajesh/sample/NullReference.kt
+++ src/com/rajesh/sample/NullReference.kt
@@ -6,4 +6,14 @@ fun main(args: Array<String>){
 
     var str2: String = str?: "Null Value"
     println(str2?.toUpperCase())
+
+    val str4: String ?= "This is not NULL"
+    str4?.let { println(it) }
+
+    //var str3: String = str!!.toUpperCase()
+    //println(str3)
+
+    val names = arrayOfNulls<String?>(3)
+    println(names[1])
+
 }
