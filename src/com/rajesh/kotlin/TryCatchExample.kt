8461bacbc556e4fce6aa5dfda5ffa1867ef82a02
Rajesh <rajeshkannang87@gmail.com>
2020-05-06 09:54:48 +0530

    Kotlin Try/Catch example

---
+++ src/com/rajesh/kotlin/TryCatchExample.kt
@@ -0,0 +1,22 @@
+package com.rajesh.kotlin
+
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
+fun main(args: Array<String>){
+    println(convertNumber("20a") ?: throw CustomException("Given value is not in Number Format"))
+}
+
+class CustomException(message: String) : Exception(message)
+
+fun convertNumber(str: String): Int? {
+    return try {
+        Integer.parseInt(str)
+    }catch (e: NumberFormatException){
+        null
+    }
+    finally {
+        println("I will always execute")
+    }
+}
